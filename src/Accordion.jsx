717f39a2e66f01afa80976925096866a2af89dcd
soumya <soumya.pal@webskitters.com>
2024-08-02 20:10:15 +0530

    push

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -3,17 +3,18 @@ export default function Accordion({ items, error }) {
     return <>
         {items.length === 0 && <p>{error}</p>}
         <div className="accordionSet">
-            {
-                ...[items].map((item, index) => {
+            {/* {
+               items.map(item => {
                     console.log(item);
-                    <div key={item.Id} item={item} className="accordion_item">
-                        <div className="accordion_item_title" onClick={() => { console.log(item) }}><h3>{item.Title}</h3></div>
+                    <div key={item.Id} className="accordion_item">
+                        <div className="accordion_item_title" onClick={() => { console.log(e.target) }}><h3>{item.Title}</h3></div>
                         <div className="accordion_item_content">
+                            <h4>{item.Name}</h4>
                             <p>{item.Content}</p>
                         </div>
                     </div>
                 })
-            }
+            } */}
         </div>
 
     </>
